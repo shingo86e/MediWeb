bb759ce7ee07da2d1bc886332e553ca6caa4681f
Exequiel Paz <exequielsony@gmail.com>
2025-09-18 21:19:58 -0300

    color de boton de responder de arrastrar y soltar

--- js/leccion.js
+++ js/leccion.js
@@ -362,9 +362,9 @@ function mostrarPregunta() {
         inputElem.appendChild(listaOpciones);
 
         // Botón exclusivo para validar arrastrar
-        const btnArrastrar = document.createElement('button');
-        btnArrastrar.textContent = 'Responder';
-        btnArrastrar.style.marginTop = '12px';
+    const btnArrastrar = document.createElement('button');
+    btnArrastrar.className = 'btn-responder';
+    btnArrastrar.innerHTML = 'Responder <svg xmlns="http://www.w3.org/2000/svg" width="18" height="18" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><polygon points="5 3 19 12 5 21 5 3"></polygon></svg>';
         btnArrastrar.onclick = function() {
             let respuestaUsuario = [];
             let incompleto = false;
